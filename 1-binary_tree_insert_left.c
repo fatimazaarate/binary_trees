107933be8c3a503d14114ff01898ab299bda31a7
fatimazaarate <fatima.zaarate@gmail.com>
2024-01-02 22:09:26 +0100

    left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,7 +5,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *left_child;
 
 	left_child = binary_tree_node(parent, value);
-	if (!left_child)
+	if (!parent)
 		return (NULL);
 
 	if (parent->left)
