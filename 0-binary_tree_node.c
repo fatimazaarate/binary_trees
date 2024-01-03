31d500152c42576b8b4a8e967ee7b4c93af62a9c
fatimazaarate <fatima.zaarate@gmail.com>
2024-01-03 09:07:00 +0100

    task

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,17 +10,16 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newNode;
+	binary_tree_t *new_node;
 
-	newNode = malloc(sizeof(binary_tree_t));
-	if (newNode == NULL)
-		/* handle memory allo failure */
+	new_node = (binary_tree_t *) malloc(sizeof(binary_tree_t));
+	if (!new_node)
 		return (NULL);
 
-	newNode->n = value;
-	newNode->left = NULL;
-	newNode->right = NULL;
-	newNode->parent = parent;
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
-	return (newNode);
+	return (new_node);
 }
