a286e8f90359dc2fe6b830f630b2e1e88975a6f0
fatimazaarate <fatima.zaarate@gmail.com>
2024-01-03 18:22:23 +0100

    task

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_depth - measures the depth of a node in a binary tree
  * @tree: is a pointer to the node to measure the depth
- * 
+ * Return: depth
 */
 
 size_t binary_tree_depth(const binary_tree_t *tree)
