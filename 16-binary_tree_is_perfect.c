df326f7f2bff87bc28007d192423574359734983
fatimazaarate <fatima.zaarate@gmail.com>
2024-01-04 18:33:05 +0100

    task

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -75,11 +75,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if ((binary_tree_is_full(tree->left)) &&
-	(binary_tree_is_full(tree->right)) &&
-	(binary_tree_balance(tree)) &&
-	(binary_tree_balance(tree->left)) &&
-	(binary_tree_balance(tree->right)))
+	if ((binary_tree_is_full(tree) && binary_tree_balance(tree)))
 		return (1);
 
 	return (0);
